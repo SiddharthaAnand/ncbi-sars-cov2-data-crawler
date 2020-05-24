c9a513b31c807a8a4b96edf04a51bd12e0054242
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-05-24 17:25:01 +0530

    Re-run the code with the newer query string added to get the whole atcg string

--- ncbi_sars2_crawler.py
+++ ncbi_sars2_crawler.py
@@ -1,11 +1,13 @@
-
+import re
 import time
+import json
 import selenium
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from empty_accessions import read_as_json
 
 
 def store_gnome_urls(url=None, chromepath=None):
@@ -70,16 +72,90 @@ def store_gnome_urls(url=None, chromepath=None):
     return gnome_urls_store
 
 
+def read_urls_from_serialized_json_file(file_path=None):
+    if file_path is not None:
+        with open(file_path, 'r') as data_file:
+            accession_url_mapper = json.load(data_file)
+        return accession_url_mapper
+    raise FileNotFoundError
+
+
+def store_atcg_string(base_url=None, query_param=None, accession_url_mapper=None,
+                      chromepath=None, directory=None):
+    if accession_url_mapper is not None:
+        accessions_read = 0
+        empty_read = {}
+        driver = webdriver.Chrome(chromepath)
+        for accession in accession_url_mapper:
+            driver.get(base_url + accession_url_mapper[accession] + query_param)
+            time.sleep(4)
+            atcg_page_html = BeautifulSoup(driver.page_source, features='html.parser')
+            seq_list = atcg_page_html.findAll('span', attrs={'id': re.compile(accession + '.\d+_\d+')})
+            print('Reading atcg sequence for accession %s' % accession)
+            temp_seq_store = ""
+            for seq in seq_list:
+                temp_seq_store += seq.text
+            temp_seq_store = temp_seq_store.replace(' ', '')
+            # print('Sequence read\t: %s' % temp_seq_store)
+
+            if len(temp_seq_store) == 0:
+                empty_read[accession] = accession_url_mapper[accession]
+            with open(directory + accession + '.txt', 'w') as writer:
+                writer.write(temp_seq_store)
+            accessions_read += 1
+            if accessions_read % 10 == 0:
+                print('%d/%d accessions written to file' % (accessions_read,len(accession_url_mapper)))
+
+        ######################################################################
+        #           Write empty accession in a file                         ##
+        ######################################################################
+        with open('empty_accessions_read', 'w') as writer:
+            json.dump(empty_read, writer)
+
+        ######################################################################
+        #                           Some stats                              ##
+        ######################################################################
+        print('#' * 40)
+        print('%d/%d accessions written' % (len(accession_url_mapper) - len(empty_read), len(accession_url_mapper)))
+        print('Empty accessions read \t: %d' % len(empty_read))
+        print('#' * 40)
+
+
 if __name__ == '__main__':
     import sys
-    import json
-    print("Starting time\t", time.asctime())
-    t0 = time.time()
-    complete_gnome_url_dict = store_gnome_urls(url=sys.argv[1], chromepath=sys.argv[2])
-    tf = time.time()
-    print(complete_gnome_url_dict)
-    print('Time taken for the crawl\t', (tf-t0) / 3600)
-    with open("complete_gnome_urls_store", "w") as gnome_url_data_store:
-        json.dump(complete_gnome_url_dict, gnome_url_data_store)
 
-    print(json.dumps(complete_gnome_url_dict, indent=4))
+    start_time = time.asctime()
+    t0 = time.time()
+    ##################################################################
+    #               Store genome urls from accession links          ##
+    ##################################################################
+    # url = https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Nucleotide&VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202,%20taxid:2697049&Completeness_s=complete
+    # complete_gnome_url_dict = store_gnome_urls(url=sys.argv[1], chromepath=sys.argv[2])
+    # print(complete_gnome_url_dict)
+    ##################################################################
+    #       Store the gnome urls in a file                          ##
+    ##################################################################
+    # with open("complete_gnome_urls_store", "w") as gnome_url_data_store:
+    #     json.dump(complete_gnome_url_dict, gnome_url_data_store)
+    #
+    # print(json.dumps(complete_gnome_url_dict, indent=4))
+    ##################################################################
+    #       Read stored urls and open and store atcg strings         #
+    ##################################################################
+    json_data = read_urls_from_serialized_json_file('complete_gnome_urls_store')
+    ##################################################################
+    #       Read the empty accessions and store atcg strings again   #
+    ##################################################################
+    # json_data = read_as_json(filename='empty_accessions_read')
+    store_atcg_string(base_url='https://www.ncbi.nlm.nih.gov',
+                      query_param='?expands-on=true',
+                      accession_url_mapper=json_data,
+                      chromepath=sys.argv[1],
+                      directory=sys.argv[2])
+    tf = time.time()
+    end_time = time.asctime()
+    print('Time taken for the crawl \t: %f' % ((tf - t0) / 3600))
+    print('Start time in human terms\t:%s' % start_time)
+    print('End time in human terms  \t:%s' % end_time)
+    print('#' * 40)
+
