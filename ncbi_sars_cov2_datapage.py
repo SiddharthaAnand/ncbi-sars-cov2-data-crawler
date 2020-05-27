f97333ac18d0325437e8fd37f6d09896e4bee951
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-05-27 22:04:43 +0530

    Add class which represents the genbank page

--- ncbi_sars_cov2_datapage.py
+++ ncbi_sars_cov2_datapage.py
@@ -1,5 +1,9 @@
 class NcbiSarsCov2DataPage(object):
+    """
+    URL which this class represents: https://www.ncbi.nlm.nih.gov/labs/virus/vssi/#/virus?SeqType_s=Nucleotide&
+    VirusLineage_ss=Severe%20acute%20respiratory%20syndrome%20coronavirus%202,%20taxid:2697049&Completeness_s=complete
 
+    """
     def __init__(self):
         self.nucleotide_element = ''
         self.accession_element = '[title="Expand record details"]'
@@ -11,3 +15,13 @@ class NcbiSarsCov2DataPage(object):
     @staticmethod
     def get_instance():
         return NcbiSarsCov2DataPage().__init__()
+
+
+class GenbankCompleteGenomePage(object):
+    """
+    URL which this class represents: https://www.ncbi.nlm.nih.gov/nuccore/XXXXXXX
+    where XXXXXX: is the accession id
+    """
+    def __init__(self):
+        pass
+
