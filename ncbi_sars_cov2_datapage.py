d7ae787263c454dea13c3737983ba5805aeda3d0
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-05-27 12:02:37 +0530

    Define xpaths for the elements in the class

--- ncbi_sars_cov2_datapage.py
+++ ncbi_sars_cov2_datapage.py
@@ -5,6 +5,7 @@ class NcbiSarsCov2DataPage(object):
         self.accession_element = '[title="Expand record details"]'
         self.close_button_on_details_modal = "//*[@id='cmscontent']/section/uswds-ncbi-app-root/uswds-ncbi-app-report/div/div[2]/uswds-ncbi-app-report-data/div/div[2]/div[1]/div[2]/div[1]/i"
         self.next_page_button_element = "//button[@aria-label=\"Next Page\"]"
+        self.total_pages_element = ""
         return self
 
     @staticmethod
