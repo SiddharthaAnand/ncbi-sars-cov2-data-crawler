d2803319557edba1d905bb590096f667fe2d0bab
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-04 23:15:47 +0530

    Create a method to create new directory if not present

--- serializer_deserializer.py
+++ serializer_deserializer.py
@@ -2,6 +2,12 @@ import json
 import time
 
 
+def create_directory_if_not_present(rel_path):
+    import os
+    if not os.path.exists(rel_path):
+        os.makedirs(rel_path)
+        print("Directory %s created successfully!" % rel_path)
+
 def read_as_json(filename=None):
     if filename is not None:
         print('#' * 20)
