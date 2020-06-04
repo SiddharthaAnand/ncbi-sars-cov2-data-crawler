94b80ad095891b27b61c07323b280931804ac941
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-04 23:19:49 +0530

    Move the method from main class to utility class

--- serializer_deserializer.py
+++ serializer_deserializer.py
@@ -1,12 +1,20 @@
 import json
-import time
+import os
+
+
+def serialize_accession_to_rel_url_mapper(rel_file_path=None, genome_to_url_mapper_dict=None):
+    if not os.path.exists(rel_file_path):
+        created = create_directory_if_not_present(rel_file_path)
+    if created is True:
+        with open(rel_file_path + "/genome_to_url_mapper_dict", "w") as fp:
+            json.dump(genome_to_url_mapper_dict, fp)
 
 
 def create_directory_if_not_present(rel_path):
-    import os
-    if not os.path.exists(rel_path):
-        os.makedirs(rel_path)
-        print("Directory %s created successfully!" % rel_path)
+    os.makedirs(rel_path)
+    print("Directory %s created successfully!" % rel_path)
+    return os.path.exists(rel_path)
+
 
 def read_as_json(filename=None):
     if filename is not None:
