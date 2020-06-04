7d933ad5c6c5a6b7bfc857c30e5b8efc73718727
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-04 23:30:13 +0530

    Move the method from main class to utility for modularity

--- serializer_deserializer.py
+++ serializer_deserializer.py
@@ -2,6 +2,21 @@ import json
 import os
 
 
+def read_urls_from_serialized_json_file(rel_file_path=None):
+    """
+    Read the relative urls from the serialized json file.
+    Those urls contain the atcg sequence data.
+    :param rel_file_path: The path to the file which stores the serialized url.
+    :return:
+    """
+    if not os.path.exists(rel_file_path):
+        print('FILE PATH DOES NOT EXIST TO BE READ:\t %s' % rel_file_path)
+        raise FileNotFoundError
+    with open(rel_file_path + "/genome_to_url_mapper_dict", 'r') as data_file:
+        accession_url_mapper = json.load(data_file)
+    return accession_url_mapper
+
+
 def serialize_metadata_of_nucleotide(rel_file_path=None, nucleotide_details_dict=None):
     """
     Store metadata for the nucleotide
