769484d4647d38e32ec69b08f6460a6fb0b95888
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-09 22:01:13 +0530

    Refactor code and update file paths

--- convert_dict_to_csv.py
+++ convert_dict_to_csv.py
@@ -1,15 +1,16 @@
-def get_json_from_file(filename='nucleotide_details_dict'):
-    import json
-    with open('data/third_run/' + filename, 'r') as fp:
+import json
+import time
+
+def get_json_from_file(rel_file_path=None):
+    with open(rel_file_path + "/nucleotide_details_dict", 'r') as fp:
         x = json.load(fp)
     return x
 
 
-def dict_to_csv(filename='metadata_20200531.csv', dict={}):
-    with open('data/third_run/' + filename, 'w') as fp:
+def dict_to_csv(rel_file_path='20200531.csv', dict={}):
+    with open(rel_file_path + "/metadata_" + str(time.time()) + ".csv", 'w') as fp:
         fp.write('Accession,Collection Date,Geo Location\n')
         for i in dict:
-            temp = ""
             if 'Geo Location' not in dict[i]:
                 temp = i + "," + dict[i]['Collection Date'] + "," + "NULL" + '\n'
             else:
