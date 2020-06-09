107f1497269604ef318e0bd849f9e0536a26d2b0
SiddharthaAnand <siddharthalibra13@gmail.com>
2020-06-09 22:04:41 +0530

    Move out the  method to a utility method

--- convert_dict_to_csv.py
+++ convert_dict_to_csv.py
@@ -1,24 +1,3 @@
-import json
-from datetime import datetime
-
-
-def get_json_from_file(rel_file_path=None):
-    with open(rel_file_path + "/nucleotide_details_dict", 'r') as fp:
-        x = json.load(fp)
-    return x
-
-
-def dict_to_csv(rel_file_path=None, dict={}):
-    with open(rel_file_path + "/metadata_" + datetime.today().strftime('%Y-%m-%d') + ".csv", 'w') as fp:
-        fp.write('Accession,Collection Date,Geo Location\n')
-        for i in dict:
-            if 'Geo Location' not in dict[i]:
-                temp = i + "," + dict[i]['Collection Date'] + "," + "NULL" + '\n'
-            else:
-                temp = i + "," + dict[i]['Collection Date'] + "," + dict[i]['Geo Location'] + '\n'
-            fp.write(temp)
-        print(temp)
-
 
 if __name__ == '__main__':
     dict = get_json_from_file()
